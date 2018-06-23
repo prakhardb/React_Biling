570d0a4118f1ba56ecaebb29706a769622d8658f
prakhar dubey <prakhardb10@gmail.com>
2018-06-23 21:12:10 +0530

    Loading sample

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,25 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component{
+    state = {
+        fishes: {},
+        order: {}
+    };
+    addFish = fish => {
+       //Take a copy of the exsiting state
+       const fishes ={...this.state.fishes };
+       // Add our new fish to fishes variable
+       fishes[`fish${Date.now()}`] = fish;
+       /// Set the new fishes object to state
+       this.setState({ fishes });
+    };
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes })
+    };
+
     render(){
         return (
             <div className="catch-of-the-day">
@@ -11,7 +28,7 @@ class App extends React.Component{
             <Header tagline="Fresh Seafood Market" />
             </div>
             <Order />
-            <Inventory />
+            <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         );
     }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,25 @@
 import React, {Fragment} from 'react';
-
+import { getFunName } from '../helpers';
 class StorePicker extends React.Component{
+//    constructor() {
+//        super();
+//        this.goToStore = this.goToStore.bind(this)
+//    }
+    myInput = React.createRef();
+    goToStore =event => {
+        //Stop The form from submiting
+        event.preventDefault();
+        // get the text from the input
+        const storeName = this.myInput.value.value;
+        // change page to store/:storeId
+        this.props.history.push(`/store/${storeName}`);
+    }; 
     render(){
         return (
         <Fragment>  
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
            <h2>Please Enter A Store</h2>
-           <input type="text" required placeholder="Store Name"/>
+           <input ref={this.myInput} type="text" required placeholder="Store Name" defaultValue={ getFunName() } />
            <button type="submit">Visit Store →</button>
          </form>
          </Fragment>  
