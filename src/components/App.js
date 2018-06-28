7170e3dbc40e56dadc8524ee56d400014555af7f
prakhar dubey <prakhardb10@gmail.com>
2018-06-28 23:58:19 +0530

    Order is save in local storage

--- src/components/App.js
+++ src/components/App.js
@@ -14,10 +14,20 @@ class App extends React.Component{
 
     componentDidMount(){
         const { params } = this.props.match;
+        const localStorageRef = localStorage.getItem(params.storeId);
+        if(localStorageRef) {
+            this.setState({ order: JSON.parse(localStorageRef) })
+        }
        this.ref = base.syncState(`${params.storeId}/fishes`, {
            context: this,
            state: 'fishes'
        });
+    }
+
+    
+    componentDidUpdate() {
+        const params = this.props.match.params;
+        localStorage.setItem(params.storeId, JSON.stringify(this.state.order));
     }
 
     componentWillUnmount () {
